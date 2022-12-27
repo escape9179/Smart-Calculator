66341e35e077aac60af51f4a3e46f0b5eb5abdbb
escape9179 <tr3.logan@gmail.com>
2022-12-26 23:14:16 -0500

    Add two numbers

---
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -0,0 +1,10 @@
+package calculator
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val num1 = scanner.nextInt()
+    val num2 = scanner.nextInt()
+    println(num1 + num2)
+}
