26571fb549ab4c38bf16b8c275cf0c7860f27964
escape9179 <tr3.logan@gmail.com>
2023-01-04 00:26:06 -0500

    Might've added variable support

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -3,8 +3,12 @@ package calculator
 import java.util.Scanner
 import kotlin.system.exitProcess
 
+private const val VARIABLE_REGEX = "[A-Za-z]+"
 private val variableMap = mutableMapOf<String, Int>()
-private const val INVALID_INPUT_MESSAGE = "Invalid expression"
+private const val INVALID_ASSIGNMENT_MESSAGE = "Invalid assignment"
+private const val UNKNOWN_VARIABLE_MESSAGE = "Unknown variable"
+private const val INVALID_IDENTIFIER_MESSAGE = "Invalid identifier"
+private const val INVALID_EXPRESSION_MESSAGE = "Invalid expression"
 private const val BYE_MESSAGE = "Bye!"
 private const val INPUT_DELIMITER = ' '
 private const val PLUS_SIGN = "+"
@@ -13,13 +17,13 @@ private const val MULTIPLY_SIGN = "*"
 private const val DIVIDE_SIGN = "/"
 private const val COMMAND_PREFIX = "/"
 private const val EQUALS_SIGN = "="
-private const val VARIABLE_ASSIGNMENT_REGEX = "[A-Za-z]+ *= *([0-9]+|[A-Za-z]+)$"
+private const val VARIABLE_ASSIGNMENT_REGEX = "$VARIABLE_REGEX *= *([0-9]+|[A-Za-z]+)$"
 
 fun main() {
     val scanner = Scanner(System.`in`)
     /* Keep reading input until the user enters a command or a new line is entered. */
     while (scanner.hasNextLine()) {
-        val input = scanner.nextLine()
+        var input = scanner.nextLine()
         /* Process whichever command the user entered
         * then go back to input processing. */
         if (input.startsWith(COMMAND_PREFIX)) {
@@ -31,20 +35,70 @@ fun main() {
             continue
         }
 
+        if (EQUALS_SIGN.toRegex().findAll(input).count() > 1) {
+            println(INVALID_ASSIGNMENT_MESSAGE)
+            continue
+        }
+
+        var parts: List<String> // Parts of the equation separated by '='
+        var lhs = "" // The left-hand side of the equation
+        var rhs = ""// The right-hand side of the equation
+        if (EQUALS_SIGN in input) {
+            parts = input.split(EQUALS_SIGN).map { it.trim() }
+            lhs = parts[0]
+            rhs = parts[1]
+        }
+
+        if (lhs.contains("([A-Za-z]+\\d+|\\d+[A-Za-z]+)".toRegex())) {
+            println(INVALID_IDENTIFIER_MESSAGE)
+            continue
+        }
         /* Check if the input is a variable assignment. If it is, then
         * split the string at the '=' and put the variable and value in
         * the variable map. */
         if (VARIABLE_ASSIGNMENT_REGEX.toRegex().matches(input)) {
-            println("This is variable assignment")
-            val parts = input.split(EQUALS_SIGN)
-            val variable = parts[0].trim()
-            val value = parts[1].trim().toInt()
-            variableMap[variable] = value
+            /* If the value being assigned to the variable is also a variable
+            * and that same variable is also in the variable map, then assign
+            * the variable on the left side of the equation the value of the
+            * variable in the right. */
+            if ("[A-Za-z]+".toRegex().matches(rhs)) {
+                if (rhs in variableMap) {
+                    variableMap[lhs] = variableMap[rhs]!!
+                    continue
+                } else {
+                    /* If the variable on the right side of the expression
+                    * isn't in the variable map, then print an error message. */
+                    println(UNKNOWN_VARIABLE_MESSAGE)
+                    continue
+                }
+            }
+            /* Assume that the expression on the right side of the equation
+            * is an integer since the variable regex couldn't match it and
+            * assign the variable that integer. */
+            variableMap[lhs] = rhs.toInt()
+            continue
+        } else if (EQUALS_SIGN in input) {
+            println(INVALID_ASSIGNMENT_MESSAGE)
             continue
         }
-
         if (!isValidExpression(input)) {
-            println(INVALID_INPUT_MESSAGE)
+//            println(INVALID_EXPRESSION_MESSAGE)
+            println(INVALID_IDENTIFIER_MESSAGE)
+            continue
+        }
+        /* Replace all variables in the expression with their numerical values,
+        * if they are present within the variable map. If the variable isn't present
+        * send an error message. */
+        var hasUnknownVariable = false
+        "\\b[a-zA-Z]+\\b".toRegex().findAll(input).forEach {
+            if (variableMap[it.value] == null) {
+                hasUnknownVariable = true
+                return@forEach
+            }
+            input = input.replace(it.value, variableMap[it.value].toString())
+        }
+        if (hasUnknownVariable) {
+            println(UNKNOWN_VARIABLE_MESSAGE)
             continue
         }
         /* Split input string into tokens (separated by spaces)
@@ -81,6 +135,9 @@ fun main() {
     }
 }
 
+/**
+ *
+ */
 private fun isValidExpression(input: String): Boolean {
     /* If the input ends with an +, -, *, or / then the expression is invalid. */
     if (input.endsWith(PLUS_SIGN)
@@ -90,10 +147,9 @@ private fun isValidExpression(input: String): Boolean {
     ) {
         return false
     }
-    /* The first alternation of the regex checks if there are two numbers separated by a space
-    * and the second alternation checks if there are any latin characters.
-    * Both alternations shouldn't be present in the string and therefore return false if present. */
-    if ("(\\d+ \\d+|[A-Za-z]+)".toRegex().containsMatchIn(input)) return false
+    if ("\\d+ \\d+|[a-zA-Z]+\\d+".toRegex().containsMatchIn(input)) {
+        return false
+    }
     return true
 }
 
